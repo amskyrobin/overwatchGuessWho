d98a3c9e8403a0b33a7806f567feb403febb0ae4
Amy Robinson <amskyrobinson@gmail.com>
2017-03-11 20:47:20 +0000

    hero renders name

--- client/src/Components/Hero.jsx
+++ client/src/Components/Hero.jsx
@@ -3,21 +3,19 @@ import React from 'react';
 
 const Hero = (props) => {
 
-  const heroes = props.heroData.map(function(hero, index){
-    return (
-      <div className='overwatchHero'>
-      <h1>{hero.name}</h1>
-      </div>  
-      );
+  return (
+    <div>
+      {props.heroData.map((hero, index) => {
+        return (
+          <h1 key={index}>{hero.name}</h1>
+          )
+        })}
+    </div>
 
-    return (
-      <div>
-        {heroes}
-      </div>
-      )
-
-  });
+    )
 }
+
+
 
 
 
